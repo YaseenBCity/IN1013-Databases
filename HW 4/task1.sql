ea931259beae3a9f27373d8d777302b6a078881a
YaseenBCity <Yaseen.Barlas@city.ac.uk>
2024-10-21 12:32:12 +0100

    Homework done

---
+++ HW 4/task1.sql
@@ -0,0 +1,17 @@
+CREATE TABLE petPet (
+	petName VARCHAR(20),
+	owner VARCHAR(45),
+	species VARCHAR(45),
+	gender VARCHAR(20),
+	birth DATE,
+	death DATE
+);
+
+CREATE TABLE petEvent (
+	petName VARCHAR(20),
+	eventdate DATE,
+	eventtype VARCHAR(15),
+	remark VARCHAR(255),
+);
+
+
---
+++ HW 4/task2.sql
@@ -0,0 +1,29 @@
+ALTER TABLE petPet
+ADD PRIMARY KEY (petName);
+
+ALTER TABLE petEvent
+ADD FOREIGN KEY (petName) REFERENCES petPet(petName);
+
+INSERT INTO petEvent( petName, eventdate, eventtype, remark)
+VALUES ('Fluffy', '2020-10-15', 'vet', 'antibiotics');
+
+INSERT INTO petPet( petName, owner, species, gender, birth, death)
+VALUES ('Hammy', 'Diane', 'hamster','M','2010-10-30',NULL );
+
+INSERT INTO petEVent( petName, eventdate, eventtype, remark)
+VALUES ('Hammy','2020-10-15','vet','antibiotics');
+
+UPDATE petEvent
+SET remark ='5 kittens, 2 male, 3 female'
+WHERE petName ='Fluffy';
+
+UPDATE petEvent
+SET petName = 'Claws'
+WHERE eventdate ='1997-08-03';
+
+UPDATE petPet
+SET death = '2020-09-01'
+WHERE petName = 'Puffball';
+
+DELETE FROM petEvent WHERE petName ='Buffy';
+DELETE FROM petPet WHERE petName = 'Buffy';
